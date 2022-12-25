8ec01588aba2e44d141ec3e852bdeccfcef522e2
Oleg Ivanov <o.ivanov@g.nsu.ru>
2022-12-25 18:12:33 +0700

    8

--- task_12/src/main/java/SortThread.java
+++ task_12/src/main/java/SortThread.java
@@ -1,16 +1,12 @@
 public class SortThread implements Runnable{
     private MyList myList;
-    private Boolean isRunning;
     public SortThread(MyList myList){
         this.myList = myList;
-        this.isRunning=true;
     }
-    public void end(){
-        this.isRunning=false;
-    }
+
     @Override
     public void run(){
-        while (isRunning){
+        while (true){
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
--- task_8/src/main/java/CounterManager.java
+++ task_8/src/main/java/CounterManager.java
@@ -3,12 +3,13 @@
 import java.util.ArrayList;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class CounterManager {
     private class Counter implements Callable<Double> {
-        private Integer threadCnt;
-        private Integer iterationsCnt;
-        private Integer threadNum; // from zero
+        private final int threadCnt;
+        private long iterationsCnt;
+        private final int threadNum; // from zero
         public Counter(int threadCnt,int threadNum){
             this.threadCnt=threadCnt;
             this.iterationsCnt=0;
@@ -16,54 +17,55 @@ public class CounterManager {
         }
         @Override
         public Double call() throws Exception {
-            int foldingNum = this.threadNum;
+            long foldingNum = this.threadNum;
             double res=0.0;
             while(isRunning){
                 res+=(foldingNum%2==0?1:-1)*(1/((double)foldingNum*2+1));
+                //System.out.println(res);
+                //System.out.println((foldingNum%2==0?1:-1)*(1/((double)foldingNum*2+1)));
                 this.iterationsCnt++;
                 foldingNum+=this.threadCnt;
                 //System.out.println(threadNum+ " " + this.iterationsCnt);
             }
-            max_iterations_Cnt.set(max_iterations_Cnt.get()>this.iterationsCnt?max_iterations_Cnt.get():this.iterationsCnt);
-            //System.out.println(1);
+            max_iterations_Cnt.set(Math.max(max_iterations_Cnt.get(), this.iterationsCnt));
             barrier.await();
-            //System.out.println(2);
 
-            for(int i=this.iterationsCnt;i<=max_iterations_Cnt.get();i++){
+            for(long i=this.iterationsCnt;i<=max_iterations_Cnt.get();i++){
                 res+=(foldingNum%2==0?1:-1)*(1/((double)foldingNum*2+1));
                 foldingNum+=this.threadCnt;
             }
-            System.out.println(threadNum + "- ended calc ");
+            System.out.println(threadNum + "- ended calc " );
             return res;
         }
     }
 
 
-    private int threadCnt;
-    private  AtomicInteger max_iterations_Cnt;
-    private  Boolean isRunning;
-    private CyclicBarrier barrier;
+    private final int threadCnt;
+    private final AtomicLong max_iterations_Cnt;
+    private volatile boolean isRunning;
+    private final CyclicBarrier barrier;
     private  ArrayList<Future<Double>> counterResults;
-    private ExecutorService executor;
+
     public CounterManager(int threadCnt){
         this.threadCnt=threadCnt;
-        this.max_iterations_Cnt=new AtomicInteger(0);
+        this.max_iterations_Cnt=new AtomicLong(0);
         this.isRunning=true;
         this.barrier = new CyclicBarrier(threadCnt);
     }
     public void performCalculations() throws ExecutionException, InterruptedException {
-        this.executor = Executors.newFixedThreadPool(this.threadCnt);
+        ExecutorService executor = Executors.newFixedThreadPool(this.threadCnt);
         this.counterResults=new ArrayList<>();
         for (int i=0;i<this.threadCnt;i++){
             Counter counter=new Counter(this.threadCnt,i);
-            this.counterResults.add(i,executor.submit(counter));
+            this.counterResults.add(i, executor.submit(counter));
         }
         executor.shutdown();
     }
-    Double getRes() throws ExecutionException, InterruptedException {
-        Double res=0.0;
+    double getRes() throws ExecutionException, InterruptedException {
+        double res=0.0;
         this.isRunning=false;
         for(int i=0;i<this.threadCnt;i++){
+            //System.out.println(counterResults.get(i).get());
             res+=counterResults.get(i).get();
         }
         return res;
